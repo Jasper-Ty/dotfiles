358fa118409600588098246ef3b6f30b64e01d98
Jasper Ty <jasperty2@gmail.com>
2023-08-31 14:54:16 -0400

    create scripts directory and move polybar launch script

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-CONFIG="$HOME/.config/polybar/config.ini"
-LOG_FILE="/tmp/polybar.log"
+CONFIG="/home/jasper/dotfiles/polybar/config.ini"
+LOG="/tmp/polybar.log"
 
 # Terminate already running polybar instances
 killall -q polybar
 
 # Touch polybar log
-echo "---" >> "$LOG_FILE"
+echo "---" >> "$LOG"
 
 # Main monitor 
 export POLYBAR_MONITOR=eDP
 export POLYBAR_TRAY_POSITION=right
 export POLYBAR_PADDING_RIGHT=2
-polybar --config="$CONFIG" top >> "$LOG_FILE" 2>&1 & disown
-polybar --config="$CONFIG" bottom >> "$LOG_FILE" 2>&1 & disown
+polybar --config="$CONFIG" top >> "$LOG" 2>&1 & disown
+polybar --config="$CONFIG" bottom >> "$LOG" 2>&1 & disown
 
 # Other monitor 
 export POLYBAR_MONITOR=HDMI-A-0
