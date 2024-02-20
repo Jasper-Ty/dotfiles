a9837015434e247f473487b1887014075898193c
Jasper <jasperty2@gmail.com>
2024-02-20 03:57:45 -0500

    Update alacritty install script to create config dir

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 source $DOTFILES/scripts/helpers.sh
 
+mkdir -p "$HOME/.config/alacritty"
+
 symlink "alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
 symlink "alacritty/themes" "$HOME/.config/alacritty/themes"
