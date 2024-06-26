68e51871110f359ab9ee712a6e74b87ac5ccbe1a
Jasper Ty <jasperty2@gmail.com>
2024-06-26 15:48:36 -0400

    Add .bash_profile
    I'm gonna be honest I'm still not 100% clear on the difference between
    .bashrc and .bash_profile.

---
+++ bash/.bash_profile
@@ -0,0 +1,2 @@
+PATH="/usr/local/texlive/2024/bin/x86_64-linux:$PATH"
+export PATH
--- bash/install.sh
+++ bash/install.sh
@@ -2,3 +2,4 @@
 source $DOTFILES/scripts/helpers.sh
 
 symlink "bash/.bashrc" "$HOME/.bashrc"
+symlink "bash/.bash_profile" "$HOME/.bash_profile"
