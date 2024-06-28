682583f3cf300b9ef77e485c0323c4a94c9ba4b0
Jasper Ty <jasperty2@gmail.com>
2024-06-28 14:39:58 -0400

    Update Alacritty install and README
    Alacritty install now checks if alacritty is installed

--- alacritty/README.md
+++ alacritty/README.md
@@ -1,5 +1,9 @@
 # Alacritty config
 
-I build alacritty from source, I'm on 0.13.0
+I build Alacritty from source. I'm currently on 0.13.0.
 
-Themes are from the alacritty themes repo
+Themes are from the [Alacritty themes repository](https://github.com/alacritty/alacritty-theme).
+
+## Notes
+
+- Alacritty must be installed manually-- install is a little *too* involved to be scripted (varying dependencies, post install steps, ...)
--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
 source $DOTFILES/scripts/helpers.sh
 
-mkdir -p "$HOME/.config/alacritty"
-mkdir -p "$HOME/.config/alacritty/themes"
+if command -v > /dev/null "alacritty"
+then
+    cecho "GREEN" "Alacritty is installed\n"
 
-symlink "alacritty/alacritty.toml"          "$HOME/.config/alacritty/alacritty.toml"
-symlink "alacritty/themes/tokyo-night.toml" "$HOME/.config/alacritty/themes/tokyo-night.toml"
+    mkdir -p "$HOME/.config/alacritty"
+    mkdir -p "$HOME/.config/alacritty/themes"
+
+    symlink "alacritty/alacritty.toml"          "$HOME/.config/alacritty/alacritty.toml"
+    symlink "alacritty/themes/tokyo-night.toml" "$HOME/.config/alacritty/themes/tokyo-night.toml"
+else
+    log_error "Alacritty is not installed"
+fi
