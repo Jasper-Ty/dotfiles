0e7952251101ec3e98ebc6be2338faa954c99465
Jasper Ty <jasperty2@gmail.com>
2024-07-02 15:44:41 -0400

    Add TeX Live directory to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+
     set -gx EDITOR nvim
+    set -gx MANPATH "$MANPATH:/usr/local/texlive/2024/texmf-dist/doc/man"
+    set -gx INFOPATH "$INFOPATH:/usr/local/texlive/2024/texmf-dist/doc/info"
+
+    # PATH
+    fish_add_path --path /usr/local/texlive/2024/bin/x86_64-linux
 end
